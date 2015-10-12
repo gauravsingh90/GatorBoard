de03fa799fa62ef81f1fe7fae84b0b0c1e11ff71
AswiniRamesh <a.ramesh@ufl.edu>
2015-10-12 12:36:58 -0400

    webservice integrated

--- app/src/main/java/com/gatorboard/gatorboard/EventBoard.java
+++ app/src/main/java/com/gatorboard/gatorboard/EventBoard.java
@@ -1,5 +1,8 @@
 package com.gatorboard.gatorboard;
 
+import android.view.View.OnClickListener;
+import java.util.ArrayList;
+import java.util.List;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,15 +11,45 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.app.ListActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.io.PrintWriter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.gatorboard.gatorboard.urlRequester;
+import com.gatorboard.gatorboard.urlConnectionManager;
+import com.gatorboard.gatorboard.eventParser;
 
 import java.util.List;
 
-public class EventBoard extends AppCompatActivity {
+public class EventBoard extends ListActivity {
 
-    public static final String EVENT_TITLE = "eventTITLE";
-    public static final String EVENT_DESC = "eventDESC";
-    public static final int EVENT_ID = 1;
+
+    private static final String PHOTOS_BASE_URL =
+            "http://services.hanselandpetal.com/photos/";
+
+
+    ProgressBar pb;
+    List<MyTask> tasks;
+
 
     protected List<Event> Events;
     @Override
@@ -25,47 +58,18 @@ public class EventBoard extends AppCompatActivity {
         setContentView(R.layout.activity_event_board);
 
 
-        Events = Events_DataProvider.getEventData();
-        ArrayAdapter<Event> EventAdapter = new ArrayAdapter<Event>(this,android.R.layout.simple_expandable_list_item_1,Events);
-        ListView listview = (ListView) findViewById(android.R.id.list);
-        listview.setAdapter(EventAdapter);
 
 
-        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Event event = Events.get(position);
-                displayDetail(event);
-            }
-        });
+        pb = (ProgressBar) findViewById(R.id.progressBar1);
+        pb.setVisibility(View.INVISIBLE);
 
 
 
-        /*Iterator<Event> iterator = Events.iterator();
-        StringBuilder builder = new StringBuilder();
-        while(iterator.hasNext()){
-            Event event = iterator.next();
-            builder.append(event.getEventId()).append(" ").append(event.getEventDesc()).append("\n");
-        }
-        TextView tv = (TextView) findViewById(R.id.EventHolder);
-        tv.setText(builder.toString());*/
+
+        tasks = new ArrayList<>();
+
+
     }
-
-    private void displayDetail(Event event) {
-        //Log.d("MainActivity", "Displaying Event: "+ event.getEventDesc());
-        Intent intent = new Intent(this,Event_Details.class);
-        intent.putExtra(EVENT_TITLE, event.getEventName());
-        intent.putExtra(EVENT_DESC, event.getEventDesc());
-        startActivityForResult(intent, EVENT_ID);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_event_board, menu);
-        return true;
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -75,9 +79,102 @@ public class EventBoard extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+       this.startWorking();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_event_board, menu);
+        return true;
+    }
+
+
+
+
+   //discuss format of data
+    public void startWorking(){
+        if (isOnline()) {
+
+            System.out.println("finally here");
+            requestData("http://services.hanselandpetal.com/secure/flowers.json");
+        } else {
+            System.out.println("inside offline");
+            Toast.makeText(this, "Network is not available", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    private void requestData(String uri) {
+
+        urlRequester p = new urlRequester();
+        p.setMethod("GET");
+        p.setUri(uri);
+        p.setParam("name", "Rosa");
+
+        MyTask task = new MyTask();
+        task.execute(p);
+    }
+
+    protected void updateDisplay() {
+        //array adapter sushma
+        //output.append(result + "\n");
+        //EventAdapter adapter = new EventAdapter(this, R.layout.item_flower, Events);
+        //setListAdapter(adapter);
+    }
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private class MyTask extends AsyncTask<urlRequester, String,  List<Event>> {
+
+        @Override
+        protected void onPreExecute() {
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.VISIBLE);
+            }
+            tasks.add(this);
+        }
+
+        @Override
+        protected List<Event> doInBackground(urlRequester... params) {
+            String content = urlConnectionManager.getData(params[0]);
+            Events = eventParser.parseFeed(content);
+            return Events;
+
+        }
+
+        @Override
+        protected void onPostExecute(List<Event> result) {
+
+            updateDisplay();
+
+            tasks.remove(this);
+            if (tasks.size() == 0) {
+                pb.setVisibility(View.INVISIBLE);
+            }
+
+
+
+        }
+
+    }
+
 }
